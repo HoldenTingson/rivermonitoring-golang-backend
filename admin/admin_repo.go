4322f4ca322d43d72dd31d024487871338374fa1
HoldenTingson <holdentingson33@gmail.com>
2025-06-04 21:02:48 +0700

    format time

--- admin/admin_repo.go
+++ admin/admin_repo.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 )
 
 type DBTX interface {
@@ -100,6 +101,7 @@ func (r *repository) GetAdminById(ctx context.Context, id int) (*Admin, error) {
 	}
 
 	admin.CreatedAt, _ = util.FormatIndonesianTimezone(admin.CreatedAt)
+	fmt.Println(admin.CreatedAt)
 
 	return &admin, nil
 }
